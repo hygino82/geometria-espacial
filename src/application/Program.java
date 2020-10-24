b6be01a91302597e1141f47f025b4e910277b6b4
Adroaldo <hygino@msn.com>
2020-10-24 09:45:48 -0300

    implementando o tronco de pirâmide

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,19 +1,21 @@
 package application;
 
-import entities.Piramide;
 import entities.Poligono;
+import entities.Tronco;
 
 public class Program {
 
 	public static void main(String[] args)  {
 		
-		//Prisma p =  new Prisma(Poligono.CIRCULO, 3.0, 15.0);
-		Piramide p =  new Piramide(Poligono.HEXAGONO_REGULAR, 3.0, 15.0);
-
+		//Prisma p =  new Prisma(Poligono.CIRCULO, 8.0, 15.0);
+		//Piramide p =  new Piramide(Poligono.TRIANGULO_EQUILATERO, 8.0, 15.0);
+		Tronco p = new Tronco(Poligono.QUADRADO, 50.0, 48.0, 30.0);
 		System.out.println(p.getPoligono());
 		System.out.println("Altura: "+p.getAltura());
-		System.out.println("Arresta da base: "+ p.getArrestaBase());
-		System.out.println("Área da base: "+ p.areaBase());
+		System.out.println("Arresta da primeira base: "+ p.getArrestaBase());
+		System.out.println("Arresta da segunda base: "+ p.getArrestaSegundaBase());
+		System.out.println("Área da primeira base: "+ p.areaBase());
+		System.out.println("Área da segunda base: "+ p.areaBase2());
 		System.out.println("Área da lateral: "+ p.areaLateral());
 		System.out.println("Área total: "+ p.areaTotal());
 		System.out.println("Volume: "+ p.volume());
--- src/entities/Piramide.java
+++ src/entities/Piramide.java
@@ -18,27 +18,32 @@ public class Piramide extends Poliedro implements GerarValores,Area
 	public Double areaLateral() {
 		double m, g;
 		double area = 0;
+		double l = getArrestaBase();
+		double h = getAltura();
+		
 		switch (getPoligono()) {
 		case CIRCULO:
-			g = Math.sqrt(Math.pow(getArrestaBase(), 2.0) + Math.pow(getAltura(), 2.0));
-			area = Math.PI * getArrestaBase() * g;
+			g = Math.sqrt(Math.pow(l, 2.0) + Math.pow(h, 2.0));
+			//System.out.println("g = " + g);
+			area = Math.PI * l * g;
 			break;
 		case QUADRADO:
-			m = getArrestaBase() / 2.0;
-			g = Math.sqrt(Math.pow(getAltura(), 2.0) + Math.pow(m, 2.0));
-			area = 4 * getArrestaBase() * g / 2.0;
+			m = l / 2.0;
+			g = Math.sqrt(Math.pow(h, 2.0) + Math.pow(m, 2.0));
+			//System.out.println("g = " + g);
+			area = 4 * l * g / 2.0;
 			break;
 		case HEXAGONO_REGULAR://erro  corrigir
-			//m = getArrestaBase() * Math.sqrt(3) / 2.0;
-			m = getArrestaBase();
-			g = Math.sqrt(Math.pow(getAltura(), 2.0) + Math.pow(m, 2.0));
+			m = l * Math.sqrt(3) / 2.0;
+			//m = getArrestaBase();
+			g = Math.sqrt(Math.pow(h, 2.0) + Math.pow(m, 2.0));
 
-			area = 6 * getArrestaBase() * g / 2.0;
+			area = 6 * l * g / 2.0;
 			break;
 		default:// triangulo equilatero
 			m = getArrestaBase() * Math.sqrt(3) / 6.0;
 			g = Math.sqrt(Math.pow(getAltura(), 2.0) + Math.pow(m, 2.0));
-			area = 3 * getArrestaBase() * g / 2.0;
+			area = 3 * l * g / 2.0;
 			break;
 		}
 		//System.out.println("g = " + g);
