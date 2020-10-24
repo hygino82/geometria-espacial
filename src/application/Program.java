8d8c65933176b3f76c7d86918608f000ae5383a3
Adroaldo <hygino@msn.com>
2020-10-24 20:51:45 -0300

    Implementado toString da classe Prisma

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,24 +1,28 @@
 package application;
 
+import java.util.Locale;
+
 import entities.Poligono;
-import entities.Tronco;
+import entities.Prisma;
 
 public class Program {
 
 	public static void main(String[] args)  {
+		Locale.setDefault(Locale.US);
 		
-		//Prisma p =  new Prisma(Poligono.CIRCULO, 8.0, 15.0);
+		Prisma p =  new Prisma(Poligono.TRIANGULO_EQUILATERO, 8.0, 15.0);
 		//Piramide p =  new Piramide(Poligono.TRIANGULO_EQUILATERO, 8.0, 15.0);
-		Tronco p = new Tronco(Poligono.QUADRADO, 50.0, 48.0, 30.0);
-		System.out.println(p.getPoligono());
-		System.out.println("Altura: "+p.getAltura());
+		System.out.println(p);
+		//Tronco p = new Tronco(Poligono.QUADRADO, 50.0, 48.0, 30.0);
+		//System.out.println(p.getPoligono());
+		/*System.out.println("Altura: "+p.getAltura());
 		System.out.println("Arresta da primeira base: "+ p.getArrestaBase());
-		System.out.println("Arresta da segunda base: "+ p.getArrestaSegundaBase());
+		//System.out.println("Arresta da segunda base: "+ p.getArrestaSegundaBase());
 		System.out.println("Área da primeira base: "+ p.areaBase());
-		System.out.println("Área da segunda base: "+ p.areaBase2());
+		//System.out.println("Área da segunda base: "+ p.areaBase2());
 		System.out.println("Área da lateral: "+ p.areaLateral());
 		System.out.println("Área total: "+ p.areaTotal());
-		System.out.println("Volume: "+ p.volume());
+		System.out.println("Volume: "+ p.volume());*/
 	}
 
 }
--- src/entities/Prisma.java
+++ src/entities/Prisma.java
@@ -48,4 +48,24 @@ public class Prisma extends Poliedro implements GerarValores, Area {
 		return 2 * areaBase() + areaLateral();
 	}
 
+	@Override
+	public String toString() {
+		String casas = "%.4f";
+		StringBuilder sb = new StringBuilder();
+		sb.append("O prisma possui como base um "+ getPoligono());
+		sb.append("\nArresta da base: " + String.format("%.4f", getArrestaBase()));
+		sb.append("\nAltura: " + String.format(casas, getAltura()));
+		sb.append("\nÁrea da base: " + String.format(casas, areaBase()));
+		sb.append("\nÁrea lateral: " + String.format(casas, areaLateral()));
+		sb.append("\nÁrea total: " + String.format(casas, areaTotal()));
+		sb.append("\nVolume: " + String.format(casas, volume()));
+		
+		
+		return sb.toString();
+		//return "Prisma [volume()=" + volume() + ", areaLateral()=" + areaLateral() + ", areaBase()=" + areaBase()
+				//+ ", areaTotal()=" + areaTotal() + "]";
+	}
+	
+	
+
 }
